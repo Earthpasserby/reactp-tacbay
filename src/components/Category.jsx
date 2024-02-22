17ee05b21ea2776433b03c21cf0f6c7ed3c8a741
Earthpasserby <iwuno68@gmail.com>
2024-02-22 16:19:55 +0100

    More features

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -189,13 +189,13 @@ const Category = () => {
               style={{ width: "205px", height: "28px" }}
             >
               <IoLocationOutline />
-              Kano, Kano
+              PortHarcort, Rivers
             </span>
             <Card.Title
               style={{ width: "378px", height: "28px" }}
               className="card-title"
             >
-              Chic Styles Hair Salon
+              IronCraft Welding Solutions
             </Card.Title>
             <Card.Text>
               <p style={{ width: "378px", height: "38px" }} className="card-p1">
@@ -246,13 +246,13 @@ const Category = () => {
               style={{ width: "205px", height: "28px" }}
             >
               <IoLocationOutline />
-              Ibadan, Oyo
+              Ilorin, Kwara
             </span>
             <Card.Title
               style={{ width: "378px", height: "28px" }}
               className="card-title"
             >
-              WonderCraft Wonders
+              MasterCuts Hair Studio
             </Card.Title>
             <Card.Text>
               <p style={{ width: "378px", height: "38px" }} className="card-p1">
@@ -287,7 +287,7 @@ const Category = () => {
               </p>
               <p>
                 {" "}
-                <RiStarSFill /> 4.5 | 85 Reviews
+                <RiStarSFill /> 4.6| 75 Reviews
               </p>
             </Card.Text>
           </Card>
@@ -299,13 +299,13 @@ const Category = () => {
               className="text-primary"
               style={{ width: "205px", height: "28px" }}
             >
-              <IoLocationOutline /> Akure, Ondo
+              <IoLocationOutline /> Benin City, Edo
             </span>
             <Card.Title
               style={{ width: "378px", height: "28px" }}
               className="card-title"
             >
-              CreativeHands Shoe Boutique
+              Chic Styles Hair Salon
             </Card.Title>
             <Card.Text>
               <p style={{ width: "378px", height: "38px" }} className="card-p1">
@@ -340,7 +340,345 @@ const Category = () => {
               </p>
               <p>
                 {" "}
-                <RiStarSFill /> 4.8 | 110 Reviews
+                <RiStarSFill /> 4.9 | 90 Reviews
+              </p>
+            </Card.Text>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col sm={4}>
+          <Card className="border border-0">
+            <Card.Img
+              src="./images/welder.png"
+              className="img-fluid card-img"
+            />
+            <span
+              className="text-primary"
+              style={{ width: "205px", height: "28px" }}
+            >
+              <IoLocationOutline />
+              PortHarcort, Rivers
+            </span>
+            <Card.Title
+              style={{ width: "378px", height: "28px" }}
+              className="card-title"
+            >
+              IronCraft Welding Solutions
+            </Card.Title>
+            <Card.Text>
+              <p style={{ width: "378px", height: "38px" }} className="card-p1">
+                Where style meets precision. Professional hairstyling and beauty
+                services for all.
+              </p>
+              <div className="d-flex">
+                <p
+                  style={{ width: "109px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Beauty Services
+                </p>
+                <p
+                  style={{ width: "83px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Hair Styling
+                </p>
+                <p
+                  style={{ width: "108px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Barber Services
+                </p>
+              </div>
+              <p
+                style={{ width: "150px", height: "20px" }}
+                className="me-2 card-p"
+              >
+                Beauty Treatments
+              </p>
+              <p>
+                {" "}
+                <RiStarSFill /> 4.8 | 104 Reviews
+              </p>
+            </Card.Text>
+          </Card>
+        </Col>
+        <Col sm={4}>
+          <Card className="border border-0">
+            <Card.Img
+              src="./images/hairstyle.png"
+              className="img-fluid card-img"
+            />
+            <span
+              className="text-primary"
+              style={{ width: "205px", height: "28px" }}
+            >
+              <IoLocationOutline />
+              Ilorin, Kwara
+            </span>
+            <Card.Title
+              style={{ width: "378px", height: "28px" }}
+              className="card-title"
+            >
+              MasterCuts Hair Studio
+            </Card.Title>
+            <Card.Text>
+              <p style={{ width: "378px", height: "38px" }} className="card-p1">
+                Where style meets precision. Professional hairstyling and beauty
+                services for all.
+              </p>
+              <div className="d-flex">
+                <p
+                  style={{ width: "109px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Beauty Services
+                </p>
+                <p
+                  style={{ width: "83px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Hair Styling
+                </p>
+                <p
+                  style={{ width: "108px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Barber Services
+                </p>
+              </div>
+              <p
+                style={{ width: "150px", height: "20px" }}
+                className="me-2 card-p"
+              >
+                Beauty Treatments
+              </p>
+              <p>
+                {" "}
+                <RiStarSFill /> 4.6| 75 Reviews
+              </p>
+            </Card.Text>
+          </Card>
+        </Col>
+        <Col sm={4}>
+          <Card className="border border-0">
+            <Card.Img src="./images/glass.png" className="img-fluid card-img" />
+            <span
+              className="text-primary"
+              style={{ width: "205px", height: "28px" }}
+            >
+              <IoLocationOutline /> Benin City, Edo
+            </span>
+            <Card.Title
+              style={{ width: "378px", height: "28px" }}
+              className="card-title"
+            >
+              Chic Styles Hair Salon
+            </Card.Title>
+            <Card.Text>
+              <p style={{ width: "378px", height: "38px" }} className="card-p1">
+                Where style meets precision. Professional hairstyling and beauty
+                services for all.
+              </p>
+              <div className="d-flex">
+                <p
+                  style={{ width: "109px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Beauty Services
+                </p>
+                <p
+                  style={{ width: "83px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Hair Styling
+                </p>
+                <p
+                  style={{ width: "108px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Barber Services
+                </p>
+              </div>
+              <p
+                style={{ width: "150px", height: "20px" }}
+                className="me-2 card-p"
+              >
+                Beauty Treatments
+              </p>
+              <p>
+                {" "}
+                <RiStarSFill /> 4.9 | 90 Reviews
+              </p>
+            </Card.Text>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="mt-3">
+        <Col sm={4}>
+          <Card className="border border-0">
+            <Card.Img
+              src="./images/welder.png"
+              className="img-fluid card-img"
+            />
+            <span
+              className="text-primary"
+              style={{ width: "205px", height: "28px" }}
+            >
+              <IoLocationOutline />
+              PortHarcort, Rivers
+            </span>
+            <Card.Title
+              style={{ width: "378px", height: "28px" }}
+              className="card-title"
+            >
+              IronCraft Welding Solutions
+            </Card.Title>
+            <Card.Text>
+              <p style={{ width: "378px", height: "38px" }} className="card-p1">
+                Where style meets precision. Professional hairstyling and beauty
+                services for all.
+              </p>
+              <div className="d-flex">
+                <p
+                  style={{ width: "109px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Beauty Services
+                </p>
+                <p
+                  style={{ width: "83px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Hair Styling
+                </p>
+                <p
+                  style={{ width: "108px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Barber Services
+                </p>
+              </div>
+              <p
+                style={{ width: "150px", height: "20px" }}
+                className="me-2 card-p"
+              >
+                Beauty Treatments
+              </p>
+              <p>
+                {" "}
+                <RiStarSFill /> 4.8 | 104 Reviews
+              </p>
+            </Card.Text>
+          </Card>
+        </Col>
+        <Col sm={4}>
+          <Card className="border border-0">
+            <Card.Img
+              src="./images/hairstyle.png"
+              className="img-fluid card-img"
+            />
+            <span
+              className="text-primary"
+              style={{ width: "205px", height: "28px" }}
+            >
+              <IoLocationOutline />
+              Ilorin, Kwara
+            </span>
+            <Card.Title
+              style={{ width: "378px", height: "28px" }}
+              className="card-title"
+            >
+              MasterCuts Hair Studio
+            </Card.Title>
+            <Card.Text>
+              <p style={{ width: "378px", height: "38px" }} className="card-p1">
+                Where style meets precision. Professional hairstyling and beauty
+                services for all.
+              </p>
+              <div className="d-flex">
+                <p
+                  style={{ width: "109px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Beauty Services
+                </p>
+                <p
+                  style={{ width: "83px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Hair Styling
+                </p>
+                <p
+                  style={{ width: "108px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Barber Services
+                </p>
+              </div>
+              <p
+                style={{ width: "150px", height: "20px" }}
+                className="me-2 card-p"
+              >
+                Beauty Treatments
+              </p>
+              <p>
+                {" "}
+                <RiStarSFill /> 4.6| 75 Reviews
+              </p>
+            </Card.Text>
+          </Card>
+        </Col>
+        <Col sm={4}>
+          <Card className="border border-0">
+            <Card.Img src="./images/glass.png" className="img-fluid card-img" />
+            <span
+              className="text-primary"
+              style={{ width: "205px", height: "28px" }}
+            >
+              <IoLocationOutline /> Benin City, Edo
+            </span>
+            <Card.Title
+              style={{ width: "378px", height: "28px" }}
+              className="card-title"
+            >
+              Chic Styles Hair Salon
+            </Card.Title>
+            <Card.Text>
+              <p style={{ width: "378px", height: "38px" }} className="card-p1">
+                Where style meets precision. Professional hairstyling and beauty
+                services for all.
+              </p>
+              <div className="d-flex">
+                <p
+                  style={{ width: "109px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Beauty Services
+                </p>
+                <p
+                  style={{ width: "83px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Hair Styling
+                </p>
+                <p
+                  style={{ width: "108px", height: "20px" }}
+                  className="me-2 card-p"
+                >
+                  Barber Services
+                </p>
+              </div>
+              <p
+                style={{ width: "150px", height: "20px" }}
+                className="me-2 card-p"
+              >
+                Beauty Treatments
+              </p>
+              <p>
+                {" "}
+                <RiStarSFill /> 4.9 | 90 Reviews
               </p>
             </Card.Text>
           </Card>
